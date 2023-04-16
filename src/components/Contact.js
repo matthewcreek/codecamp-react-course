6659f7efc41edeb18b003f9c1861e59b3e679d48
Matthew Creek <matthew.creek27@gmail.com>
2023-04-16 16:39:54 -0500

    destructured props for easier reference

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,21 +1,21 @@
 import React from "react";
 
-import telephone from '../images/telephone.png';
-import email from '../images/email.png';
+import telephoneIcon from '../images/telephone.png';
+import emailIcon from '../images/email.png';
 
-export default function Contact(props) {
-    console.log(props);
+export default function Contact({img, name, phone, email}) {
+    console.log(name);
     return (
         <div className="contact-card">
-            <img src={props.img} />
-            <h3>{props.name}</h3>
+            <img src={img} />
+            <h3>{name}</h3>
             <div className="info-group">
-                <img src={telephone} />
-                <p>{props.phone}</p>
+                <img src={telephoneIcon} />
+                <p>{phone}</p>
             </div>
             <div className="info-group">
-                <img src={email} />
-                <p>{props.email}</p>
+                <img src={emailIcon} />
+                <p>{email}</p>
             </div>
         </div>
         )
