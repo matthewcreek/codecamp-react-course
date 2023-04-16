3a12a58c43c76edfce75e0498102abd7d6fbd6ae
Matthew Creek <matthew.creek27@gmail.com>
2023-04-16 17:19:30 -0500

    practice using js within jsx

--- src/App.js
+++ src/App.js
@@ -5,27 +5,51 @@ import Joke from "./components/Joke";
 
 
 export default function App() {
+
+    const joke1 = {
+        "setup": 'I got my daughter a fridge for her birthday',
+        "punchline": "I can't wait to see her face light up when she opens it."
+    };
+    const joke2 = {
+        "setup": 'How did the hacker escape the police?',
+        "punchline": 'He just ransomware!'
+    };
+    const joke3 = {
+        "setup": "Why don't pirates travel on mountain roads?",
+        "punchline": 'Scurvy.'
+    };
+    const joke4 = {
+        "setup": 'Why do bees stay in the hive in the winter?',
+        "punchline": 'Swarm.'
+    };
+    const joke5 = {
+        "setup": "What's the best thing about Switzerland?",
+        "punchline": "I don't know, but the flag is a big plus!"
+    };
+
+    let jokeList = [joke1, joke2, joke3, joke4, joke5];
+
     return (
         <div>
            <Joke 
-                setup='I got my daughter a fridge for her birthday'
-                punchline="I can't wait to see her face light up when she opens it."
+                setup={jokeList[0].setup}
+                punchline={jokeList[0].punchline}
            />
            <Joke 
-                setup='How did the hacker escape the police?'
-                punchline='He just ransomware!'
+                setup={jokeList[1].setup}
+                punchline={jokeList[1].punchline}
            />
            <Joke 
-                setup="Why don't pirates travel on mountain roads?"
-                punchline='Scurvy.'
+                setup={jokeList[2].setup}
+                punchline={jokeList[2].punchline}
            />
            <Joke 
-                setup='Why do bees stay in the hive in the winter?'
-                punchline='Swarm.'
+                setup={jokeList[3].setup}
+                punchline={jokeList[3].punchline}
            />
            <Joke 
-                setup="What's the best thing about Switzerland?"
-                punchline="I don't know, but the flag is a big plus!"
+                setup={jokeList[4].setup}
+                punchline={jokeList[4].punchline}
            />
         </div>
         
