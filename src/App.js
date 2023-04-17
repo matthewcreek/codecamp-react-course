29d927e822668227c8e595ddb6169102478a7946
Matthew Creek <matthew.creek27@gmail.com>
2023-04-16 22:23:58 -0500

    fixed issue with images not loading

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import Hero from './components/Hero.js';
 import Card from "./components/Card.js";
 import cardData from './data.js';
 
+//photos path (unsure why this is needed)
+import katie from './images/katie-zaferes.png'
+import bike from './images/mountain-bike.png'
+import wedding from './images/wedding-photography.png'
+
 export default function App() {
 
     const cardElements = cardData.map(card => {
