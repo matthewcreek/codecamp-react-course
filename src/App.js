fb0c3ab7a459423364099800f61d184bc6575296
Matthew Creek <matthew.creek27@gmail.com>
2023-04-16 21:14:52 -0500

    moved data to a separate layer and DRY code

--- src/App.js
+++ src/App.js
@@ -1,56 +1,21 @@
 import React from "react";
 
 import Joke from "./components/Joke";
+import jokesData from "./jokeData";
 
 
 
 export default function App() {
-
-    const joke1 = {
-        "setup": 'I got my daughter a fridge for her birthday',
-        "punchline": "I can't wait to see her face light up when she opens it."
-    };
-    const joke2 = {
-        "setup": 'How did the hacker escape the police?',
-        "punchline": 'He just ransomware!'
-    };
-    const joke3 = {
-        "setup": "Why don't pirates travel on mountain roads?",
-        "punchline": 'Scurvy.'
-    };
-    const joke4 = {
-        "setup": 'Why do bees stay in the hive in the winter?',
-        "punchline": 'Swarm.'
-    };
-    const joke5 = {
-        "setup": "What's the best thing about Switzerland?",
-        "punchline": "I don't know, but the flag is a big plus!"
-    };
-
-    let jokeList = [joke1, joke2, joke3, joke4, joke5];
+    const jokeElements = jokesData.map(joke => {
+        return <Joke 
+            setup = {joke.setup}
+            punchline={joke.punchline}
+        />
+    })
 
     return (
         <div>
-           <Joke 
-                setup={jokeList[0].setup}
-                punchline={jokeList[0].punchline}
-           />
-           <Joke 
-                setup={jokeList[1].setup}
-                punchline={jokeList[1].punchline}
-           />
-           <Joke 
-                setup={jokeList[2].setup}
-                punchline={jokeList[2].punchline}
-           />
-           <Joke 
-                setup={jokeList[3].setup}
-                punchline={jokeList[3].punchline}
-           />
-           <Joke 
-                setup={jokeList[4].setup}
-                punchline={jokeList[4].punchline}
-           />
+           {jokeElements}
         </div>
         
     )
