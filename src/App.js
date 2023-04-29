3f6f9d9f898688dd578ea750529a231d9ca030de
Matthew Creek <matthew.creek27@gmail.com>
2023-04-28 21:43:27 -0500

    conditional rendering part 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ export default function App() {
     
     return (
         <div>
-           {(messages.length > 0) && <h1>You have {messages.length} unread messages!</h1>}
+           {messages.length > 0 ? 
+           <h1>You have {messages.length} unread {messages.length === 1 ? 'message' : 'messages'}!</h1> : 
+            <h1>You're all caught up!</h1>}
         </div>
         
     )
