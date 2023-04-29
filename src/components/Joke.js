645760294d2ead4eb3bfca55f9aca82fe0726cfa
Matthew Creek <matthew.creek27@gmail.com>
2023-04-28 20:42:00 -0500

    used conditional rendering to hide joke punchlines at a button press

--- src/components/Joke.js
+++ src/components/Joke.js
@@ -1,10 +1,18 @@
 import React from "react";
 
 export default function Joke({setup, punchline}) {
+
+    const [isShown, setIsShown] = React.useState(false)
+
+    function handleClick() {
+        setIsShown(prevShown => !prevShown)
+
+    }
     return (
         <div className="joke">
             <h1>{setup}</h1>
-            <h3>{punchline}</h3>
+            <button onClick={handleClick}>Punchline</button>
+            {isShown && <h3>{punchline}</h3>}
         </div>
     )
 }
